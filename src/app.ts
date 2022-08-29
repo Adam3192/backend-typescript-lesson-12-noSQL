d91e8fab203cb560f84ba7a24eaa6a6e07759b2b
Adam3192 <jeremyadixon@hotmail.com>
2022-08-29 14:36:53 -0500

    lesson 12 noSQL

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express, { NextFunction, Request, Response } from 'express'
 import morgan from 'morgan';
 import coffeeRoutes from './routes/coffeeRoutes';
+import mongoose from 'mongoose';
 
+const connectionString: string = 'mongodb://localhost:27017/testDB';
+
+mongoose.connect(connectionString).then(
+    () => console.log('database connection successful!'), 
+    err => console.log('Error connecting to the database', err));
+    
 const app = express();
 
 app.use(morgan('dev'));
